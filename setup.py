fa222c81363f0f4b560ef1737f7b9d5cc4d1568a
Zimeng Xiong (asahi-air) <zxzimeng@gmail.com>
2025-03-04 20:57:55 -0800

    add to pypi for installation, rename to pros_sketchy

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pros_sketchy",
-    version="1.0.0",
-    scripts=['sketchy_pros_compiler'],
+    name="sketchy_pros_compiler",
+    version="1.0.1",
+    scripts=['pros_sketchy'],
     description="A build system wrapper for PROS robotics projects",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     author="Zimeng Xiong",
-    url="https://github.com/zxzimeng/sketchy_pros_compiler_wrapper",
+    url="https://github.com/zxzimeng/sketchy_pros_compiler",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
